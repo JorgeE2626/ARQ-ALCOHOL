0cc157a011dc1c4a3a269293e14782c75bc1a657
JorgeE2626 <jorge1ed1ma@gmail.com>
2024-12-09 10:36:27 -0600

    Add files via upload

---
+++ Alcohol.ino
@@ -0,0 +1,37 @@
+float valor_alcohol;
+
+void setup() { 
+  Serial.begin(9600);               
+  pinMode(13, OUTPUT);              
+  pinMode(12, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(10, OUTPUT);
+}
+
+void loop() { 
+  valor_alcohol = analogRead(A0);
+  Serial.println(valor_alcohol);  
+  
+  float porcentaje = (valor_alcohol / 1000);  
+  Serial.println(porcentaje); 
+  Serial.println(' '); 
+  
+  if (valor_alcohol > 800) {    
+    digitalWrite(13, HIGH); 
+    digitalWrite(12, HIGH); 
+  } else {                                
+    if (valor_alcohol > 500) {
+      digitalWrite(11, HIGH); 
+    } else {
+      digitalWrite(10, HIGH); 
+    }
+  }
+  
+  digitalWrite(13, LOW); 
+  delay(400);
+  digitalWrite(12, LOW); 
+  digitalWrite(11, LOW); 
+  digitalWrite(10, LOW); 
+
+  
+}
